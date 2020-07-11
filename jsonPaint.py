072fc6d0d01baef264dc7a8b7c01f1fa751f8fbe
Julian Feregotti <julainferegotti96@gmail.com>
2020-07-11 16:54:07 -0300

    Ahora tiene menu y colores

--- jsonPaint.py
+++ jsonPaint.py
@@ -30,11 +30,11 @@ def convertirDic(botones):
     # p.pprint(dic_aux)
     return dic_aux
 
-def base():
+def base(tamaño):
     dic = dict()
     
-    for x in range(15):
-        for y in range(15):
+    for x in range(tamaño):
+        for y in range(tamaño):
             dic[(x,y)] = ""
 
     base = open(os.path.join(absolute_path,"Info","base.json"),"w")
@@ -42,22 +42,21 @@ def base():
     json.dump(dic_fin,base,indent=1)
 
 #----------------------------------------------------------------------------------
+def crear_menu():
+    valores = [15 + i for i in range(10)]
+    layout_menu = [
+        [sg.Radio("facil",group_id="grupo",key="facil",default=True),sg.Radio("medio",group_id="grupo",key="medio"),sg.Radio("dificil",group_id="grupo",key="dificil")],
+        [sg.T("Tamaño de la matriz"),sg.Spin(valores,key="tam",initial_value=15)],
+        [sg.B("Dibujar",key="dibujo",size=(20,None))]
+    ]
+    return layout_menu
+#----------------------------------------------------------------------------------
 
-def crear_layout(colores,dificultad):
+def crear_layout(colore,dificultad):
     casillero = lambda name, key: sg.Button('', border_width=3, size=(3, 1), key=key,
-                                         pad=(0, 0), button_color=('black',"white"))
+                                         pad=(0, 0), button_color=("white",colore[dificultad][name]if name != "blanco" else colore[dificultad][""]))
 
-    # casilleros con letras de una partida anterior:
-
-    ficha_pc = lambda name,key: sg.Button(name, disabled=True, border_width = 3, size = (3,1), key = key, pad = (0,0), button_color = ("#000000","green"),
-                                            disabled_button_color = ("#000000","green"))
-
-    # blanco = lambda name, key: sg.Button(name, border_width=1, size=(3, 1), key=key,
-    #                                      pad=(0, 0), button_color=('black', 'white'))
-
-    ficha_jugador = lambda name, key: sg.Button(name, disabled=True,border_width=3, size=(3, 1), key=key,
-                                         pad=(0, 0), disabled_button_color=('black', "blue"),button_color=('black', "blue"))
-
+    print(colore[dificultad]["++++"])
     base = open(os.path.join(absolute_path,"Info","base.json"),"r")
     tablero = json.load(base)
     tab = convertirDic(tablero)
@@ -65,6 +64,7 @@ def crear_layout(colores,dificultad):
     # p = pprint.PrettyPrinter(indent=2)
     # p.pprint(tab)
     layout = []
+    layout_fichas = []
     largo = int(math.sqrt(len(tab)))
     botones = dict()
     for x in range(largo):
@@ -95,50 +95,85 @@ def crear_layout(colores,dificultad):
             elif tab[key] == "---":
                 fichas.append(casillero('---', key))
                 botones[key] = "---"
-            # casilleros que ya están ocupados por letras (caso de partida previamente guardada)
-            elif (tab[key][0] in string.ascii_uppercase) and (tab[key] != " "):
-                if (len(tab[key])> 1):
-                    if tab[key][1] == "*":
-                        fichas.append(ficha_pc(tab[key][0],key)) # casillas ocupadas por la maquina en una partida previa fueron guardadas con *
-                    else:
-                        fichas.append(ficha_jugador(tab[key][0],key))  # casillas ocupadas por el jugador en una partida previa
-                botones[key] = ""
-        layout.append(fichas)
+            
+        layout_fichas.append(fichas)
     tipo_ficha = ["blanco","---","--","-","+","++","+++","++++"]
     frame_layout = [
-        [sg.InputCombo(tipo_ficha,key="col")],[sg.Button("Guardar",key="g")]
+        [casillero(name,name)for name in tipo_ficha],
+        [sg.Button("Guardar",key="g")]
     ]
-    frame = [sg.Frame("cosa",layout=frame_layout)]
+    columna_datos = []
+    premio_y_descuento = {'': 'Simple', '+': 'Duplica puntaje por letra', '++': 'Triplica puntaje por letra', '+++': 'Duplica puntaje por palabra', '++++': 'Triplica puntaje por palabra', '-': 'Resta 1 pto', '--': 'Resta 2 ptos', '---': 'Resta 3 ptos'}
+    info_colores = list(map(lambda x: [sg.Button(button_color=('white',colore[dificultad][x]), size=(3,1), disabled=True), sg.Text(premio_y_descuento[x])], colore[dificultad].keys()))
+
+    for i in range(len(info_colores)):
+        columna_datos.append(info_colores[i])
+    col = [sg.Column(columna_datos)]
+    frame = [sg.Frame("Botones",layout=frame_layout)]
+    columna_principal = [
+        [sg.Col(layout_fichas)]+col
+    ]
+    layout.extend(columna_principal)
     layout.append(frame)
     return layout, botones
 
 
+sg.LOOK_AND_FEEL_TABLE['MyNewTheme'] = {'BACKGROUND': '#c5dbf1',
+                                    'TEXT': '#000000',
+                                    'INPUT': '#2a6daf',
+                                    'TEXT_INPUT': '#000000',
+                                    'SCROLL': '#2a6daf',
+                                    'BUTTON': ('white', '#2a6daf'),
+                                    'PROGRESS': ('#2a6daf', '#2a6daf'),
+                                    'BORDER': 0, 'SLIDER_DEPTH': 0, 'PROGRESS_DEPTH': 0,
+                                    }
+
+sg.theme("MyNewTheme")
+
 colores =  open(os.path.join(absolute_path,"info","colores.json"),"r")
 col = json.load(colores)
-dificultad = sg.popup_get_text("facil medio dificil")
-layout,botones = crear_layout(colores,dificultad)
-window = sg.Window("asd",layout)
+
+window_menu  = sg.Window("Dibujo",crear_menu())
 while True:
-    event,  values =  window.read()
-    print(event)
-    if (event == None):
+    event, values = window_menu.read()
+    if (event ==  None):
         break
-    if event in botones.keys():
-        ind = event
-        valor =values["col"]
-        print(valor)
-        if valor == "blanco":
-            window[ind].update("",button_color=("white","white"))
-        else:
-            window[ind].update(valor,button_color=('black',col[dificultad][valor]))
-        botones[ind] = valor if valor != "blanco" else ""
-    if event == "g":
-        nombre = sg.popup_get_text("Ingrese nombre de json")
-        dic = convertirJson(botones)
-        datos = open(os.path.join("Export",nombre+".json"),"w")
-        json.dump(dic,datos)
-        datos.close()
+    
+    if (event == "dibujo"):
+        if (window_menu["facil"].get()):
+            dificultad = "facil"
+        elif (window_menu["medio"].get()):
+            dificultad = "medio"
+        elif (window_menu["dificil"].get()):
+            dificultad = "dificil"
+        tamaño = int(window_menu["tam"].get())
+        base(tamaño)
+        window_menu.close()
+
+
+        layout,botones = crear_layout(col,dificultad)
+        window_2 = sg.Window("asd",layout)
+        tipos = ["-","--","---","+","++","+++","++++","blanco"]
+        while True:
+            event,  values =  window_2.read()
+            print(event)
+            if (event == None):
+                break
+            if (event in tipos):
+                color = col[dificultad][event] if event != "blanco" else col[dificultad][""]
+                ind = event
+                ficha = event if event != "blanco" else ""
+            if event in botones.keys():
+                ind = event
+                window_2[ind].update(ficha,button_color=('black',color))
+                botones[ind] = ficha
+            if event == "g":
+                nombre = sg.popup_get_text("Ingrese nombre de json")
+                dic = convertirJson(botones)
+                datos = open(os.path.join("Export",nombre+".json"),"w")
+                json.dump(dic,datos,indent=1)
+                datos.close()
 
     
-window.close()
+        window_2.close()
             
